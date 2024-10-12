7b4b07fa44aad695dc3fe35a2c3a3da7a41b6d5f
Sanjay Kanwar <sanj.kan123@gmail.com>
2024-10-12 11:10:18 +1100

    popup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { ReactFlow, useNodesState, useEdgesState } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { buildTeams } from "./utils/index";
@@ -15,6 +15,9 @@ const { nodes: initialNodes, edges: initialEdges } = buildTeams();
 const App = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [hoveredNode, setHoveredNode] = useState(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
 
   const onConnect = useCallback(
     (params) =>
@@ -30,20 +33,49 @@ const App = () => {
       ),
     [setEdges]
   );
+
+  const onNodeMouseEnter = useCallback((event, node) => {
+    setHoveredNode(node);
+    const rect = event.target.getBoundingClientRect();
+    setMousePosition({ x: rect.right, y: rect.top });
+  });
+
+  const onNodeMouseLeave = useCallback(() => {
+    setHoveredNode(null);
+  });
   return (
-    // <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        className="react-flow-subflows-example"
-        fitView
-        // edgeTypes={edgeTypes}
-        // nodeTypes={nodeTypes}
-      />
-    // </div>
+    <div style={{ position: 'relative', width: "100%", height: "100%" }}>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onNodeMouseEnter={onNodeMouseEnter}
+      onNodeMouseLeave={onNodeMouseLeave}
+      onConnect={onConnect}
+      className="react-flow-subflows-example"
+      fitView
+      // edgeTypes={edgeTypes}
+      // nodeTypes={nodeTypes}
+    >
+      {hoveredNode?.data?.label && (
+        <div
+          style={{
+            position: "absolute",
+            zIndex: 10000,
+            top: mousePosition.y + 10, // Slight offset from the element
+            left: mousePosition.x + 10,
+            padding: "5px 10px",
+            background: "black",
+            color: "white",
+            borderRadius: "4px",
+          }}
+        >
+          {hoveredNode.data.label}
+        </div>
+      )}
+    </ReactFlow>
+     </div>
   );
 };
 export default App;
